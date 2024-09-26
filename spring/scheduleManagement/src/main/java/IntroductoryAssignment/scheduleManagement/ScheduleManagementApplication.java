1ee42e89f3934df7584cf4402091eb8eec37105b
hemgom <reborn.april23@gmail.com>
2024-09-26 16:42:04 +0900

    add new Project 'Schedule Management' & README.md

---
+++ spring/scheduleManagement/src/main/java/IntroductoryAssignment/scheduleManagement/ScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package IntroductoryAssignment.scheduleManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementApplication.class, args);
+	}
+
+}
---
+++ spring/scheduleManagement/src/test/java/IntroductoryAssignment/scheduleManagement/ScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package IntroductoryAssignment.scheduleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
