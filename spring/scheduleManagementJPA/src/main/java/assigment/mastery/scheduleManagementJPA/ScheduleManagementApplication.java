7a3a8714ae03989d06d8654fffa05317fc49619a
hemgom <reborn.april23@gmail.com>
2024-10-07 19:21:06 +0900

    add new Project 'Schedule Management JPA'

---
+++ spring/scheduleManagementJPA/src/main/java/assigment/mastery/scheduleManagementJPA/ScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package assigment.mastery.scheduleManagementJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementApplication.class, args);
+	}
+
+}
---
+++ spring/scheduleManagementJPA/src/test/java/assigment/mastery/scheduleManagementJPA/ScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package assigment.mastery.scheduleManagementJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
