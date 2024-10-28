483658bc01706e1c203b646708ce5c42e83d4589
hemgom <reborn.april23@gmail.com>
2024-10-28 16:27:39 +0900

    reflect feedback: LoggingFilter 수정

--- spring/scheduleManagementJPA/src/main/java/assigment/mastery/scheduleManagementJPA/security/filter/LoggingFilter.java
+++ spring/scheduleManagementJPA/src/main/java/assigment/mastery/scheduleManagementJPA/security/filter/LoggingFilter.java
@@ -2,11 +2,17 @@ package assigment.mastery.scheduleManagementJPA.security.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j(topic = "Logging")
 @Component
@@ -15,10 +21,22 @@ public class LoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String url = httpRequest.getRequestURI();
-        log.info("url: {}", url);
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(httpRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(httpResponse);
 
-        chain.doFilter(request, response);
+        chain.doFilter(requestWrapper, responseWrapper);
+
+        log.info("request URL: {}", httpRequest.getRequestURI());
+        log.info("authorization: {}", httpRequest.getHeader("Authorization"));
+        if (StringUtils.hasText(requestWrapper.getQueryString())) {
+            log.info("queryParameter: {}", URLDecoder.decode(requestWrapper.getQueryString(), StandardCharsets.UTF_8));
+        }
+        log.info("requestBody: \n{}", new String(requestWrapper.getContentAsByteArray()));
+
+        log.info("response status code: {}", httpResponse.getStatus());
+        log.info("responseBody: {}", new String(responseWrapper.getContentAsByteArray()));
+        responseWrapper.copyBodyToResponse();
 
         log.info("request response completed");
     }
